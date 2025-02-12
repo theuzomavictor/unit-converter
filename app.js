21ef09349b1da21ee55f95b77d9c59c93edc52d8
theuzomavictor <siruzomav96@gmail.com>
2025-02-12 12:28:18 +0100

    Add functionality to webpage
    A click event listener was added to the button element
    to run a conversion function that runs when it is clicked
    and displays the right conversions.

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const inputEl = document.getElementById("input-el")
 const btnEl = document.getElementById("btn-el")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
 
 btnEl.addEventListener("click", function() {
-    console.log("It works!")
+    let inputValue = Number(inputEl.value)
+    console.log(inputValue)
+
+    // FOR METERS
+
+    lengthEl.textContent = `
+    ${inputValue} meters = ${(inputValue*3.281).toFixed(3)} feet | ${inputValue} feet = ${(inputValue*0.305).toFixed(3)} meters
+    `
+
+    // FOR VOLUMES
+
+    volumeEl.textContent = `
+    ${inputValue} liters = ${(inputValue*0.264).toFixed(3)} gallons | ${inputValue} gallons = ${(inputValue*3.785).toFixed(3)} liters
+    `
+
+    // FOR MASS
+
+    massEl.textContent = `
+    ${inputValue} kilograms = ${(inputValue*2.204).toFixed(3)} pounds | ${inputValue} pounds = ${(inputValue*0.453).toFixed(3)} kilograms
+    `
 })
