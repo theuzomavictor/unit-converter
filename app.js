1727582e56c8d007153f03e4f2697e6fb6016b89
theuzomavictor <siruzomav96@gmail.com>
2025-02-12 11:17:41 +0100

    Connect JavaScript and test
    An event listener was added to test the button by running a function

---
+++ app.js
@@ -0,0 +1,6 @@
+const inputEl = document.getElementById("input-el")
+const btnEl = document.getElementById("btn-el")
+
+btnEl.addEventListener("click", function() {
+    console.log("It works!")
+})
